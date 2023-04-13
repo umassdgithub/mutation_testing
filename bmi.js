07bbfb22b70616315ce8adf0a41df5c13de17066
akhavan12 <amir.umassd@gmail.com>
2023-04-13 10:04:46 -0400

    update

--- bmi.js
+++ bmi.js
@@ -2,7 +2,7 @@ module.exports= function bmi(w_lbs,h_foot,h_inches) {
   // convert w to kg
   if(typeof(w_lbs)==='number' && typeof(h_foot)==='number' && typeof(h_inches)==='number')
   {
-    const w_kg = w_lbs / 2.205;
+    const w_kg = Math.abs(w_lbs / 2.205);
     const h_meter = ((h_foot*12 + h_inches) * 2.54) / 100
    // console.log(`you are ${h_meter.toFixed(2)} meters, and ${w_kg.toFixed(2)} kg`)
    // convert h from f
